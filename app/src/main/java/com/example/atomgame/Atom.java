9223b7a1ad769dd7c1807bc7f8a8fac744d814f4
Svt <sergeich1998@gmail.com>
2021-01-30 17:27:31 +0500

    handled behavior when adding and removing connection

--- app/src/main/java/com/example/atomgame/Atom.java
+++ app/src/main/java/com/example/atomgame/Atom.java
@@ -10,11 +10,13 @@ public abstract class Atom {
     private final byte atomicNumber;
     private final String name;
     private final HashSet<Connection> connections;
+    private byte numOfFreeElectrons;
 
     public Atom(byte valence, byte atomicNumber, @NonNull String name) {
         this.valence = valence;
         this.atomicNumber = atomicNumber;
         this.name = name;
+        numOfFreeElectrons = valence;
         connections = new HashSet<>();
     }
 
@@ -30,15 +32,21 @@ public abstract class Atom {
         return name;
     }
 
+    public byte getNumOfFreeElectrons() {
+        return numOfFreeElectrons;
+    }
+
     public Iterator<Connection> getConnectionIterator() {
         return connections.iterator();
     }
 
     public boolean addConnection(Connection connection) {
+        numOfFreeElectrons -= connection.getType();
         return connections.add(connection);
     }
 
     public boolean removeConnection(Connection connection) {
+        numOfFreeElectrons += connection.getType();
         return connections.remove(connection);
     }
 }
