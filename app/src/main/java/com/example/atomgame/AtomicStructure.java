1c05920c85e240b8178710e406184c772ae6300a
Svt <sergeich1998@gmail.com>
2021-02-02 14:24:17 +0500

    Changed AtomicStructure interface to abstract class

--- app/src/main/java/com/example/atomgame/AtomicStructure.java
+++ app/src/main/java/com/example/atomgame/AtomicStructure.java
@@ -1,9 +1,32 @@
 package com.example.atomgame;
 
-import java.util.Iterator;
+import androidx.annotation.NonNull;
 
-public interface AtomicStructure {
-    boolean skeletonIsCycled();
-    Iterator<Atom> getSkeletonIterator();
-    Iterator<Atom> getStructureIterator();
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public abstract class AtomicStructure {
+    private final LinkedList<Atom> skeleton;
+    private final boolean skeletonIsCycled;
+    private final HashSet<Atom> structure;
+
+    public AtomicStructure(@NonNull HashSet<Atom> structure,
+                           @NonNull LinkedList<Atom> skeleton, boolean isCycled) {
+        this.skeleton = skeleton;
+        this.structure = structure;
+        this.skeletonIsCycled = isCycled;
+    }
+
+    public boolean skeletonIsCycled() {
+        return skeletonIsCycled;
+    }
+
+    public Iterator<Atom> getSkeletonIterator() {
+        return skeleton.iterator();
+    }
+
+    public Iterator<Atom> getStructureIterator() {
+        return structure.iterator();
+    }
 }
--- app/src/main/java/com/example/atomgame/PickedAtomicStructure.java
+++ app/src/main/java/com/example/atomgame/PickedAtomicStructure.java
@@ -6,30 +6,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class PickedAtomicStructure implements AtomicStructure{
-    private final LinkedList<Atom> skeleton;
-    private final boolean skeletonIsCycled;
-    private final HashSet<Atom> structure;
+public class PickedAtomicStructure extends AtomicStructure{
 
     public PickedAtomicStructure(@NonNull HashSet<Atom> structure,
                                  @NonNull LinkedList<Atom> skeleton, boolean isCycled) {
-        this.skeleton = skeleton;
-        this.structure = structure;
-        this.skeletonIsCycled = isCycled;
-    }
-
-    @Override
-    public boolean skeletonIsCycled() {
-        return skeletonIsCycled;
-    }
-
-    @Override
-    public Iterator<Atom> getSkeletonIterator() {
-        return skeleton.iterator();
-    }
-
-    @Override
-    public Iterator<Atom> getStructureIterator() {
-        return structure.iterator();
+        super(structure, skeleton, isCycled);
     }
 }
