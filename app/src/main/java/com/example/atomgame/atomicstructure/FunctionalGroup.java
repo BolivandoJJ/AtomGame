2bb41fb9dd98c1674c2a4518c5ae2466dbe8e714
Svt <sergeich1998@gmail.com>
2021-02-06 13:03:04 +0500

    Now a molecule can have at most one functional group. Also, id field has been added to FunctionalGroup

--- app/src/main/java/com/example/atomgame/atomicstructure/FunctionalGroup.java
+++ app/src/main/java/com/example/atomgame/atomicstructure/FunctionalGroup.java
@@ -8,9 +8,15 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class FunctionalGroup extends AtomicStructure {
+    private final byte id;
 
-    public FunctionalGroup(@NonNull HashSet<Atom> atomSet, @NonNull String name,
+    public FunctionalGroup(@NonNull HashSet<Atom> atomSet, @NonNull String name, byte id,
                            @NonNull ArrayList<Atom> skeleton, boolean isCycled) {
         super(atomSet, name, skeleton, isCycled);
+        if (id >= 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("Id of functional group in molecule must be non-negative");
+        }
     }
 }
--- app/src/main/java/com/example/atomgame/atomicstructure/Molecule.java
+++ app/src/main/java/com/example/atomgame/atomicstructure/Molecule.java
@@ -1,27 +1,23 @@
 package com.example.atomgame.atomicstructure;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.atomgame.atom.Atom;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 
 public class Molecule extends AtomicStructure {
-    private final ArrayList<HashSet<FunctionalGroup>> functionalGroups;
+    private final FunctionalGroup functionalGroup;
 
-    public Molecule(@NonNull HashSet<Atom> atomSet, @NonNull ArrayList<HashSet<FunctionalGroup>> functionalGroups,
+    public Molecule(@NonNull HashSet<Atom> atomSet, @Nullable FunctionalGroup functionalGroup,
                     @NonNull ArrayList<Atom> skeleton, boolean isCycled, @NonNull String name) {
         super(atomSet, name, skeleton, isCycled);
-        this.functionalGroups = functionalGroups;
+        this.functionalGroup = functionalGroup;
     }
 
-    public ArrayList<Iterator<FunctionalGroup>> getFunctionalGrouplIteratorList() {
-        ArrayList<Iterator<FunctionalGroup>> functionalGroupIteratorList = new ArrayList<>();
-        for (HashSet<FunctionalGroup> functionalGroupSet : functionalGroups) {
-            functionalGroupIteratorList.add(functionalGroupSet.iterator());
-        }
-        return functionalGroupIteratorList;
+    public FunctionalGroup getFunctionalGroup() {
+        return functionalGroup;
     }
 }
