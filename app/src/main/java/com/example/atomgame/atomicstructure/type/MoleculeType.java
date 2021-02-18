17e033fa21cf644adf51511ed7ca4a5bda88ecb9
Svt <sergeich1998@gmail.com>
2021-02-18 22:35:11 +0500

    added field "isSimple" in MoleculeType

--- app/src/main/java/com/example/atomgame/atomicstructure/type/MoleculeType.java
+++ app/src/main/java/com/example/atomgame/atomicstructure/type/MoleculeType.java
@@ -1,35 +1,40 @@
 package com.example.atomgame.atomicstructure.type;
 
 public enum MoleculeType {
-    WATER(false),
-    CARBON_DIOXIDE(false),
-    AMMONIA(false),
-    HYDROGEN_PEROXIDE(false),
-    NITROXYL(false),
-    OZONE(false),
-    NITROGEN(false),
-    OXYGEN(false),
-    HYDRAZINE(false),
+    WATER(false, true),
+    CARBON_DIOXIDE(false, true),
+    AMMONIA(false, true),
+    HYDROGEN_PEROXIDE(false, true),
+    NITROXYL(false, true),
+    OZONE(false, true),
+    NITROGEN(false, true),
+    OXYGEN(false, true),
+    HYDRAZINE(false, true),
+    ALKANE(true, false),
+    ALKENE(true, false),
+    ALKYNE(true, false),
+    DIENE(true, false),
+    ETHER(false, false),
+    PEROXIDE(false, false),
+    IMINE(false, false),
+    AMINE(false, false),
+    AMIDE(false, false),
+    ESTER(false, false),
+    CARBONATE(false, false),
+    ANHYDRIDE(false, false);
 
-    ALKANE(true),
-    ALKENE(true),
-    ALKYNE(true),
-    DIENE(true),
-    ETHER(false),
-    PEROXIDE(false),
-    IMINE(false),
-    AMINE(false),
-    AMIDE(false),
-    ESTER(false),
-    CARBONATE(false),
-    ANHYDRIDE(false);
-
-    private final boolean mayHaveFunctionalGroup;
-    MoleculeType(boolean mayHaveFunctionalGroup) {
-        this.mayHaveFunctionalGroup = mayHaveFunctionalGroup;
+    private final boolean isMayHaveFunctionalGroup;
+    private final boolean isSimple;
+    MoleculeType(boolean isMayHaveFunctionalGroup, boolean isSimple) {
+        this.isMayHaveFunctionalGroup = isMayHaveFunctionalGroup;
+        this.isSimple = isSimple;
     }
 
     public boolean isMayHaveFunctionalGroup() {
-        return mayHaveFunctionalGroup;
+        return isMayHaveFunctionalGroup;
+    }
+
+    public boolean isSimple() {
+        return isSimple;
     }
 }
