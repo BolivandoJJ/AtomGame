b429bceadc294df2c95fb82a09a684c6db61bc60
Svt <sergeich1998@gmail.com>
2021-02-24 14:44:46 +0500

    Worked on AtomicStructureFactory

--- app/src/main/java/com/example/atomgame/AtomicStructureFactory.java
+++ app/src/main/java/com/example/atomgame/AtomicStructureFactory.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.atomgame.atom.Atom;
+import com.example.atomgame.atom.H;
 import com.example.atomgame.atomicstructure.Molecule;
 import com.example.atomgame.atomicstructure.FunctionalGroup;
+import com.example.atomgame.atomicstructure.type.MoleculeType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,11 +17,10 @@ import java.util.NoSuchElementException;
 
 public class AtomicStructureFactory {
     private final ArrayList<Atom> skeleton;
-    private byte[][] skeletonTemplate;
     private final boolean skeletonIsCycled;
-    private HashSet<Atom> atomSet;
-    private ArrayList<HashSet<FunctionalGroup>> functionalGroups;
+    private ArrayList<HashSet<FunctionalGroup>> listOfFunctionalGroupSet;
     private byte status;
+    private Molecule molecule;
     public static final byte DEFAULT_STATUS = 0;
     public static final byte MOLECULE_NOT_FOUND_STATUS = -1;
     public static final byte NEEDS_A_FUNCTIONAL_GROUP_SELECTION_STATUS = 1;
@@ -31,19 +32,24 @@ public class AtomicStructureFactory {
         status = DEFAULT_STATUS;
     }
 
+    // TODO: change logic
     private void parseSkeleton() {
-        createSkeletonTemplate();
+        byte[][] skeletonMatrix = createSkeletonMatrix();
+        boolean parsingWasSuccessful;
         if (skeletonIsCycled) {
-            parseCycledSkeleton();
+            parsingWasSuccessful = parseCycledSkeleton();
         } else {
-            parseLinearSkeleton();
+            parsingWasSuccessful = parseLinearSkeleton();
+        }
+        if (!parsingWasSuccessful) {
+            status = MOLECULE_NOT_FOUND_STATUS;
         }
     }
 
-    private void createSkeletonTemplate() {
-        byte[][] template = new byte[skeleton.size()][];
-        template[0] = new byte[1];
-        template[0][0] = skeleton.get(0).getAtomicNumber();
+    private byte[][] createSkeletonMatrix() {
+        byte[][] matrix = new byte[skeleton.size()][];
+        matrix[0] = new byte[1];
+        matrix[0][0] = skeleton.get(0).getAtomicNumber();
         Iterator<Connection> iterator;
         Connection connection;
         for (int i = 1; i < skeleton.size(); i++) {
@@ -55,9 +61,9 @@ public class AtomicStructureFactory {
             } catch (NoSuchElementException e) {
                 throw new IllegalArgumentException("Unassociated skeleton of a molecule", e);
             }
-            template[i] = new byte[i+1];
-            template[i][i] = skeleton.get(i).getAtomicNumber();
-            template[i][i-1] = connection.getType();
+            matrix[i] = new byte[i+1];
+            matrix[i][i] = skeleton.get(i).getAtomicNumber();
+            matrix[i][i-1] = connection.getType();
         }
         // set connection first and last atoms in template
         if (skeletonIsCycled) {
@@ -69,16 +75,19 @@ public class AtomicStructureFactory {
             } catch (NoSuchElementException e) {
                 throw new IllegalArgumentException("Skeleton is not cycled", e);
             }
-            template[skeleton.size() - 1][0] = connection.getType();
+            matrix[skeleton.size() - 1][0] = connection.getType();
         }
-        skeletonTemplate = template;
+        return matrix;
     }
 
+    // TODO: change logic
     private boolean parseCycledSkeleton() {
         boolean parsingWasSuccessful;
-        if (parsingWasSuccessful = parseHeterocyclicMolecule()) {
+        if (parsingWasSuccessful = parseSimpleMolecule()) {
             return parsingWasSuccessful;
-        } else if (parsingWasSuccessful = parseHomoCyclicMolecule()) {
+        } else if (parsingWasSuccessful = parseHeterocyclicMolecule()) {
+            return parsingWasSuccessful;
+        } else if (parsingWasSuccessful = parseHomocyclicMolecule()) {
             return parsingWasSuccessful;
         } else if (parsingWasSuccessful = parseFunctionalGroup()) {
             return parsingWasSuccessful;
@@ -87,6 +96,7 @@ public class AtomicStructureFactory {
         }
     }
 
+    // TODO: change logic
     private boolean parseLinearSkeleton() {
         boolean parsingWasSuccessful;
         if (parsingWasSuccessful = parseSimpleMolecule()) {
@@ -102,29 +112,100 @@ public class AtomicStructureFactory {
         }
     }
 
-    private boolean parseSimpleMolecule() {
-        
+    private boolean parseSimpleMolecule(@NonNull byte[][] skeletonMatrix) {
+        Iterator<AtomicStructureTemplate<MoleculeType>> iterator = AtomicStructureTemplate.getSimpleMoleculeSetIterator();
+        AtomicStructureTemplate<MoleculeType> template;
+        while (iterator.hasNext()) {
+            template = iterator.next();
+            if (compare2dArrays(template.getConnectionMatrix(), skeletonMatrix)) {
+                molecule = createMolecule(null, template);
+                status = MOLECULE_CREATED_STATUS;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Molecule createMolecule(@Nullable FunctionalGroup functionalGroup,
+                                    @NonNull AtomicStructureTemplate<MoleculeType> sourceTemplate,
+                                    @NonNull byte... radicalLengths) {
+        HashSet<Atom> atomSet = createAtomSet(functionalGroup);
+        AtomicStructureTemplate<MoleculeType> moleculeFormula = createMoleculeFormula(sourceTemplate, radicalLengths);
+        String name = MoleculeNameCreator.createMoleculeName(moleculeFormula, skeletonIsCycled, functionalGroup);
+        return new Molecule(atomSet, functionalGroup, skeleton, skeletonIsCycled, name, moleculeFormula);
+    }
+
+    private AtomicStructureTemplate<MoleculeType> createMoleculeFormula(@NonNull AtomicStructureTemplate<MoleculeType> sourceTemplate,
+                                                                        @NonNull byte... radicalLengths) {
+        byte[][] matrix = sourceTemplate.getConnectionMatrix();
+        Arrays.sort(radicalLengths);
+        // checking input for negative values
+        for (byte radicalLength : radicalLengths) {
+            if (radicalLength < 0) {
+                throw new IllegalArgumentException("The radical length must be non-negative");
+            }
+        }
+        // checking number of input values and adding lengths of radicals to matrix
+        byte numOfRadicals = 0;
+        for (byte i = 0; i < matrix.length; i++) {
+            if (matrix[i][i] == AtomicStructureTemplate.RADICAL) {
+                numOfRadicals++;
+                try {
+                    matrix[i][i] = (byte) -radicalLengths[i];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new IllegalArgumentException("The number of length parameters less than the number of radicals", e);
+                }
+            }
+        }
+        if (radicalLengths.length > numOfRadicals) {
+            throw new IllegalArgumentException("The number of length parameters exceeds the number of radicals");
+        }
+        return new AtomicStructureTemplate<>(matrix, sourceTemplate.getType());
+    }
+
+    private HashSet<Atom> createAtomSet(@Nullable FunctionalGroup functionalGroup) {
+        HashSet<Atom> atomSet = new HashSet<>();
+        // adding functional group atoms to set
+        if (functionalGroup != null) {
+            Iterator<Atom> iterator = functionalGroup.getAtomSetIterator();
+            while(iterator.hasNext()) {
+                atomSet.add(iterator.next());
+            }
+        }
+        // adding skeleton atoms and connected hydrogen to atomSet
+        for (Atom atom : skeleton) {
+            atomSet.add(atom);
+            Iterator<Connection> iterator = atom.getConnectionIterator();
+            while (iterator.hasNext()) {
+                Atom[] connectedAtoms = iterator.next().getConnectedAtoms();
+                for (Atom connectedAtom : connectedAtoms) {
+                    if (connectedAtom.getAtomicNumber() == H.ATOMIC_NUMBER) {
+                        atomSet.add(connectedAtom);
+                    }
+                }
+            }
+        }
+        return atomSet;
     }
 
     public byte getStatus() {
         return status;
     }
 
-    @Nullable
+
     public ArrayList<Iterator<FunctionalGroup>> getFunctionalGroups() {
-        if (functionalGroups != null) {
+        if (status == NEEDS_A_FUNCTIONAL_GROUP_SELECTION_STATUS) {
             ArrayList<Iterator<FunctionalGroup>> functionalGroupIteratorList = new ArrayList<>();
-            for (HashSet<FunctionalGroup> functionalGroupSet : functionalGroups) {
+            for (HashSet<FunctionalGroup> functionalGroupSet : listOfFunctionalGroupSet) {
                 functionalGroupIteratorList.add(functionalGroupSet.iterator());
             }
             return functionalGroupIteratorList;
+        } else {
+            return null;
         }
     }
 
-    public Molecule getMolecule(@NonNull FunctionalGroup functionalGroups) {
-
-    }
-
+    // method returns true if arrays are equals
     private static boolean compare2dArrays(@NonNull byte[][] array1, @NonNull byte[][] array2) {
         boolean arraysAreEquals = true;
         if (array1.length == array2.length) {
